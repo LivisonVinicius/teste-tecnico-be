677511fffae09ca06ba89adec3941e318cfdca44
Livison Vinícius <livisongc@gmail.com>
2024-02-25 02:30:37 -0300

    First Commit

---
+++ start/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const providers = [
+  "@adonisjs/framework/providers/AppProvider",
+  "@adonisjs/framework/providers/ViewProvider",
+  "@adonisjs/lucid/providers/LucidProvider",
+  "@adonisjs/bodyparser/providers/BodyParserProvider",
+  "@adonisjs/cors/providers/CorsProvider",
+  "@adonisjs/shield/providers/ShieldProvider",
+  "@adonisjs/session/providers/SessionProvider",
+  "@adonisjs/auth/providers/AuthProvider",
+];
+
+const aceProviders = [
+  "@adonisjs/lucid/providers/MigrationsProvider",
+  "@adonisjs/session/providers/SessionProvider",
+];
+
+const aliases = {};
+
+const commands = [];
+
+module.exports = { providers, aceProviders, aliases, commands };
